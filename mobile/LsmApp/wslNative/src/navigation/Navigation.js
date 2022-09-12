28248a9a1c2f7b32e6048e94b67d550417a8d5ce
Ivan Gonzalez <hal308366@gmail.com>
2022-09-11 23:07:11 -0700

    Fix - Tab Navigation

--- mobile/LsmApp/wslNative/src/navigation/Navigation.js
+++ mobile/LsmApp/wslNative/src/navigation/Navigation.js
@@ -7,10 +7,13 @@ const Tab = createBottomTabNavigator();
 export default function Navigation() {
   return (
     <Tab.Navigator>
-      <Tab.Screen 
+      <Tab.Screen
         name="Account"
         component={AccountNavigation}
+        options={{
+          tabBarLabel: "Mi cuenta",
+        }}
       />
     </Tab.Navigator>
-  )
+  );
 }
