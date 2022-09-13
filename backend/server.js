f8d715cf85375c3cc915eb0f93a67103194c593d
ivanozono <75452913+ivanozono@users.noreply.github.com>
2022-09-12 19:06:29 -0700

    se agregar router y se usa req.body y req.query

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,34 @@
 const express = require ('express');
+const bodyParser = require ('body-parser');
+const router = express.Router();
 
 var app = express ();
-app.use('/', function (req,res){
-    res.send('Holaaa al servidopr de wsl');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use (router);
+
+router.get ('/words', function (req,res){
+    console.log(req.body);
+    console.log(req.query);
+    res.send('Desde al servidopr de wsl ' + req.body.text + ' desde router get' );
+});
+
+router.post ('/words', function (req,res){
+    res.send('Holaaa al servidopr de wsl desde router post');
+});
+
+router.put ('/words', function (req,res){
+    res.send('Holaaa al servidopr de wsl desde router put');
+});
+
+router.patch ('/words', function (req,res){
+    res.send('Holaaa al servidopr de wsl desde router patch');
+});
+
+router.delete ('/words', function (req,res){
+    res.send('Holaaa al servidopr de wsl desde router delete');
 });
 
 app.listen(3000);
 console.log('La aplicacion esta funcionando en http://localhost:3000 ');
+
