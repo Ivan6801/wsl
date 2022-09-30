a534b9a5c5a100963b3cafbb4adde0228b72799a
ivanozono <75452913+ivanozono@users.noreply.github.com>
2022-09-29 21:18:45 -0700

    rutas y capa de red

--- backend/network/response.js
+++ backend/network/response.js
@@ -7,7 +7,8 @@ exports.success = function (req, res, message, status){
         body: message});
 }
 
-exports.error = function (req, res, message, status){
+exports.error = function (req, res, message, status, details){
+    console.error(details);
     res.status(status || 400 ).send
     ({
         error: message,
--- backend/server.js
+++ backend/server.js
@@ -1,43 +1,14 @@
 const express = require ('express');
 const bodyParser = require ('body-parser');
-const response = require ('./network/response');
-const router = express.Router();
+
+const router = require ('./network/routes');
 
 
 var app = express ();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-app.use (router);
+router(app);
 
-router.get ('/words', function (req,res){
-    console.log(req.body);
-    console.log(req.query);
-    response.success(req, res, 'List of words',200);
-   
-});
-
-router.post ('/words', function (req,res){
-    if (req.query.error=="ok"){
-        response.error(req, res, ' simulated error');
-    } else {
-        response.success(req, res, ' word created',201);
-    }
-   
-    
-});
-
-router.put ('/words', function (req,res){
-    response.success(req, res, 'word updated',200);
-   
-});
-
-router.patch ('/words', function (req,res){
-    response.success(req, res, 'word changed',200);
-});
-
-router.delete ('/words', function (req,res){
-    response.success(req, res, 'words deleted',200);
-});
 
 app.use('/app',express.static('public'));
 
